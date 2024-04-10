4d2a7db302df927db51be79044c81f5ee956228d
TuoYingtao <tuoyingtao@163.com>
2024-04-10 12:13:22 +0800

    update(mysql): 更新mysqlbinlog文件导入、mysql初始化脚本
    由于是阉割版的镜像需要手动去导入mysqlbinlog工具

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+echo '--------------- 查看mysql工具 ---------------'
+echo `ls -l /usr/bin/my*`
+
 # 查看mysql服务的状态，方便调试，这条语句可以删除
 echo `service mysql status`
 
-echo '1.启动mysql....'
+echo '--------------- 启动mysql ---------------'
 # 启动mysql
 service mysql start
 sleep 3
@@ -12,15 +15,21 @@ sleep 3
 echo `service mysql status`
 
 # 建库与角色权限
-echo '2.建库与角色权限....'
+echo '--------------- 查看配置变量 ---------------'
+mysql < SHOW VARIABLES
+sleep 2
+echo '--------------- 查看配置变量完毕 ---------------'
+
+# 建库与角色权限
+echo '--------------- 建库与角色权限 ---------------'
 mysql < /mysql/privileges.sql
 sleep 3
-echo '2-1.建库与角色权限完毕....'
+echo '--------------- 建库与角色权限完毕 ---------------'
 
 # 导入数据
-echo '3.whale-tik-cloud 库开始导入数据表结构....'
+echo '--------------- 库开始导入数据表结构 ---------------'
 mysql < /mysql/whale-tik-cloud.sql
-echo '3-1.whale-tik-cloud 库导入数据完毕....'
+echo '--------------- 库导入数据完毕 ---------------'
 
 # sleep 3
 echo `service mysql status`
